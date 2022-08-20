b5751143e13210655d53915ed4279133bd537a50
irinapodoprigora <ukrira@gmail.com>
2022-08-20 16:34:37 +0100

    "remove duplicates from sorted array" was added

---
+++ src/main/java/easy/tasks/RemoveDuplicatesFromArray.java
@@ -0,0 +1,19 @@
+package easy.tasks;
+//https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+public class RemoveDuplicatesFromArray {
+    public int removeDuplicates(int[] nums) {
+      int uniqueIndex = 0;
+      for(int i = 0; i < nums.length-1; i++){
+          //if numbers are different we continue iterating
+          // if they are not we change only current index, but unique index does not change
+          // when we met next unique number we put it in the place with only unique numbers
+          // and continue this operating with every member
+          if(nums[i] != nums[i+1]){
+              nums[uniqueIndex]= nums[i];
+              uniqueIndex++;
+          }
+      }
+       return uniqueIndex;
+    }
+}
