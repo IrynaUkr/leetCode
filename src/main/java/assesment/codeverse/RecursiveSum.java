9da2b163aa581500cb56f88e01b14a58795a5bef
irinapodoprigora <ukrira@gmail.com>
2022-12-29 22:00:38 +0000

    easy tasks assessments recursion Sum second version added

--- src/main/java/assesment/codeverse/RecursiveSum.java
+++ src/main/java/assesment/codeverse/RecursiveSum.java
@@ -2,7 +2,7 @@ package assesment.codeverse;
 
 public class RecursiveSum {
     public static int recSum(int[] numbers) {
-        return rec(numbers, 0);
+        return recDesc(numbers, numbers.length-1);
     }
 
     public static int rec(int[] arr, int index) {
@@ -11,6 +11,16 @@ public class RecursiveSum {
         }
         return arr[index] + rec(arr, index + 1);
     }
+    // desc order
+    public static int recDesc(int[] arr, int index) {
+        if (index == -1) {
+            return 0;
+        }
+        int a = arr[index] + recDesc(arr, index -1);
+        System.out.println(a);
+        return a;
+    }
+
 
     public static void main(String[] args) {
         int[] num = new int[]{10, 2, 3, 90};
